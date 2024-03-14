de2e71cc137943fe7193dc61b8114183b38aa0e2
philpln <phil.spxm@gmail.com>
2024-03-14 18:23:32 +0100

    removed dockerfiles
    Due to macnotesapp dependency on mac only packages docker does not work

--- dockering.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build Docker image
-docker build -t testing01 .
-
-# Run Docker container
-docker run -v ./Users/phil/Documents/Projekte/preisDateien testing01
-
-#-v ./Users/phil/Documents/Projekte/preisDateien -p 8000:5000 
